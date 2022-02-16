ffbac69dd8b6dc513a21eedcd415a156cc696718
Ando Lalaina <59050819+4rtw@users.noreply.github.com>
2022-02-16 14:29:39 +0300

    update index and procfile

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get("/factures", factureService.getFactures);
 app.put("/facture/calculer/:id", factureService.calculerFacture);
 app.put("/facture/payer/:id", factureService.payerFacture);
 
-const port = process.env.APP_PORT;
+const port = process.env.APP_PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Application démarré sur le port ${port}`);
